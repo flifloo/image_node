356f25ef3322a827055f83ab27f05a8c7455f716
flifloo <flifloo@gmail.com>
2020-04-26 18:55:21 +0200

    Add user login and setup extand pug template

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let express = require("express");
 let morgan = require("morgan");
+let cookieParser = require("cookie-parser");
+let bodyParser = require('body-parser');
+let session = require("express-session");
 let fs = require("fs");
-let rl = require("readline").createInterface({input: process.stdin, output: process.stdout, terminal: false});
 let passwordHash = require('password-hash');
-let app = express();
 
+let app = express();
 
 if (!fs.existsSync("users.json")) {
 	fs.writeFileSync("users.json", "{}");
@@ -16,6 +18,7 @@ if (!fs.existsSync("users.json")) {
 }
 
 async function addUser() {
+	let rl = require("readline").createInterface({input: process.stdin, output: process.stdout, terminal: false});
 	let username;
 	let password;
 	file = JSON.parse(fs.readFileSync("users.json"));
@@ -28,12 +31,49 @@ async function addUser() {
 }
 
 
-app.use(morgan("dev"))
-.use(express.static("public"))
-.set("view engine", "pug")
-.get("/", (req, res) => {
-	res.render("index");
-});
+function isAuth(req, res, next) {
+	if (req.session.login) {
+		next();
+	} else {
+		res.redirect("/login");
+	}
+}
 
-app.listen(8080)
+
+app.use(morgan("dev"))
+	.use(express.static("public"))
+	.use(bodyParser.urlencoded({ extended: true }))
+	.use(cookieParser())
+	.use(session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: { secure: "auto" }
+	}))
+	.set("trust proxy", 1)
+	.set("view engine", "pug")
+	.get("/", isAuth, (req, res) => {
+		res.render("index");
+	})
+	.get("/login", (req, res) => {
+		let fail = false;
+		if ("fail" in req.query) {
+			fail = true;
+		}
+		res.render("login", {title: "login", fail: fail});
+	}).post("/login", (req, res) => {
+		if ("username" in req.body && "password" in req.body) {
+			file = JSON.parse(fs.readFileSync("users.json"));
+			if (req.body.username in file && passwordHash.verify(req.body.password, file[req.body.username])) {
+				req.session.login = true;
+				req.session.save();
+				res.redirect("/");
+			} else {
+				res.redirect("/login?fail");
+			}
+		} else {
+			res.redirect("/login?fail");
+		}
+	})
+	.listen(8080);
 
